a61846f0fed3b39d3a08a828bd18966d1158beb1
Ruslan Pavlov <rpavlop@ubic.texh>
2025-01-30 20:04:26 +0600

    some fix

--- src/test/java/com/aviatickets/flight/FlightApplicationTests.java
+++ src/test/java/com/aviatickets/flight/FlightApplicationTests.java
@@ -1,9 +1,15 @@
 package com.aviatickets.flight;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.yml")
 class FlightApplicationTests {
 
     @Test
