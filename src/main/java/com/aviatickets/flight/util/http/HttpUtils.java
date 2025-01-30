320c8f68606f89d6b4553807810e78bc0fa0a9d2
nagichev <nagich.ev@yandex.ru>
2025-01-30 16:57:14 +0300

    Добавлен checkstyle.xml

--- src/main/java/com/aviatickets/flight/util/http/HttpUtils.java
+++ src/main/java/com/aviatickets/flight/util/http/HttpUtils.java
@@ -1,8 +1,6 @@
 package com.aviatickets.flight.util.http;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import java.util.Collection;
