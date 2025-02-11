b5fe3e3d6082646d4771447ad56d119d992f2658
Ruslan Pavlov <rpavlov@ubic.tech>
2025-02-11 18:59:14 +0600

    some fix

--- src/main/resources/db/changelog/migrations/2025_29_01_alter_airport_table.sql
+++ src/main/resources/db/changelog/migrations/2025_29_01_alter_airport_table.sql
@@ -1,5 +1,5 @@
 alter table flight_airport
     drop column city,
-    add city_id bigint not null,
+    add city_id bigint,
     add constraint fk_airport_city_id foreign key (city_id) references flight_city (id);
 
